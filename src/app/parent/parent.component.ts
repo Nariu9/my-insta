7e6f396a048c8db0ed984dcab5aa427983c96285
Artem Karpukhin <a.karpukhin9@gmail.com>
2022-12-07 16:45:51 +0200

    feat: ngSwitch usage

--- src/app/parent/parent.component.ts
+++ src/app/parent/parent.component.ts
@@ -1,46 +1,10 @@
 import { Component } from '@angular/core'
 
-interface LessonStorage {
-  id: number
-  title: string
-  weekGrades: Array<{ id: number; gradeItem: number }>
-}
-
 @Component({
   selector: 'inst-parent',
   templateUrl: './parent.component.html',
   styleUrls: ['./parent.component.scss'],
 })
 export class ParentComponent {
-  isLoading = true
-  lessons: LessonStorage[] = [
-    {
-      id: 0,
-      title: 'Math',
-      weekGrades: [
-        { id: 0, gradeItem: 5 },
-        { id: 1, gradeItem: 2 },
-        { id: 2, gradeItem: 3 },
-      ],
-    },
-    {
-      id: 1,
-      title: 'English',
-      weekGrades: [
-        { id: 0, gradeItem: 5 },
-        { id: 1, gradeItem: 5 },
-        { id: 2, gradeItem: 5 },
-      ],
-    },
-  ]
-
-  constructor() {
-    setTimeout(() => {
-      this.isLoading = false
-    }, 3000)
-  }
-
-  getGrades(value: string) {
-    //this.grade.push(value)
-  }
+  value = ''
 }
