82b20fe4267b1647b97dd878abd8b80f7a7ca5c1
Artem Karpukhin <a.karpukhin9@gmail.com>
2022-12-07 21:41:29 +0200

    feat: finish test task using ngFor, ngIf, ngClass

--- src/app/parent/parent.component.ts
+++ src/app/parent/parent.component.ts
@@ -1,18 +1,34 @@
 import { Component } from '@angular/core'
 
+interface fruitData {
+  id: string
+  name: string
+  price: number
+}
+
+// Проработка ngFor, ngIf, ngClass
+// 1. Отрисовать массив фруктов (вывести name price)
+// 2. Начётные элементы массива отобразить зелёным цветом
+// 3. Чётные элементы массива отобразить красным цветом
+// 4. Отобразить только фрукты стоимостью больше 7
+// 5. Фруктам стоимостью больше 15 добавить любой стиль,
+// который их выделит в списке, например жирный шрифт или больший размер шрифта
+
 @Component({
   selector: 'inst-parent',
   templateUrl: './parent.component.html',
   styleUrls: ['./parent.component.scss'],
 })
 export class ParentComponent {
-  isOk = false
-
-  styles = { color: 'blue', margin: '20px', opacity: '0.5' }
-
-  constructor() {
-    setTimeout(() => {
-      this.isOk = true
-    }, 3000)
-  }
+  fruits: fruitData[] = [
+    { id: '1', name: 'apples', price: 10 },
+    { id: '2', name: 'oranges', price: 20 },
+    { id: '3', name: 'watermelons', price: 30 },
+    { id: '4', name: 'bananas', price: 5 },
+    { id: '5', name: 'pears', price: 12 },
+    { id: '6', name: 'raspberries', price: 18 },
+    { id: '7', name: 'avocados', price: 14 },
+    { id: '8', name: 'mangoes', price: 3 },
+    { id: '9', name: 'kiwifruits', price: 7 },
+  ]
 }
