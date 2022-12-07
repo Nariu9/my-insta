c041af7b47b8f34af0a245bc3b207a441060f8cc
Artem Karpukhin <a.karpukhin9@gmail.com>
2022-12-07 09:27:03 +0200

    feat: ngFor usage with complex data structure

--- src/app/parent/parent.component.ts
+++ src/app/parent/parent.component.ts
@@ -1,14 +1,39 @@
 import { Component } from '@angular/core'
 
+interface LessonStorage {
+  id: number
+  title: string
+  weekGrades: Array<{ id: number; gradeItem: number }>
+}
+
 @Component({
   selector: 'inst-parent',
   templateUrl: './parent.component.html',
   styleUrls: ['./parent.component.scss'],
 })
 export class ParentComponent {
-  grade: string[] = []
+  lessons: LessonStorage[] = [
+    {
+      id: 0,
+      title: 'Math',
+      weekGrades: [
+        { id: 0, gradeItem: 5 },
+        { id: 1, gradeItem: 2 },
+        { id: 2, gradeItem: 3 },
+      ],
+    },
+    {
+      id: 1,
+      title: 'English',
+      weekGrades: [
+        { id: 0, gradeItem: 5 },
+        { id: 1, gradeItem: 5 },
+        { id: 2, gradeItem: 5 },
+      ],
+    },
+  ]
 
   getGrades(value: string) {
-    this.grade.push(value)
+    //this.grade.push(value)
   }
 }
