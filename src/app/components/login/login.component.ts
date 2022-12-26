777514c54c655bc99ec035f58ba35e907b1b2999
Artem Karpukhin <a.karpukhin9@gmail.com>
2022-12-26 08:35:36 +0200

    feat: several validators usage (13.5)

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -8,7 +8,7 @@ import { FormControl, FormGroup, Validators } from '@angular/forms'
 })
 export class LoginComponent {
   loginForm = new FormGroup({
-    email: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.required, Validators.email]),
     password: new FormControl(''),
   })
 
