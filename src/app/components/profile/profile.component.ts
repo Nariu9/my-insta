796651e1908192255bcafa54600d9add9b841ad9
Artem Karpukhin <a.karpukhin9@gmail.com>
2022-12-29 22:19:48 +0200

    feat: implement some options of back navigation (14.7)

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core'
-import { ActivatedRoute } from '@angular/router'
+import { ActivatedRoute, Router } from '@angular/router'
 import { ProfileResponse, ProfileService } from '../../services/profile.service'
 import { Observable } from 'rxjs'
 
@@ -11,12 +11,20 @@ import { Observable } from 'rxjs'
 export class ProfileComponent implements OnInit {
   profile$!: Observable<ProfileResponse>
 
-  constructor(private route: ActivatedRoute, private profileService: ProfileService) {}
+  constructor(
+    private route: ActivatedRoute,
+    private profileService: ProfileService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     const userId = Number(this.route.snapshot.paramMap.get('userId'))
     if (userId) {
       this.profile$ = this.profileService.getProfile(userId)
     }
+  }
+
+  backToUsersHandler() {
+    this.router.navigate(['/users'])
   }
 }
